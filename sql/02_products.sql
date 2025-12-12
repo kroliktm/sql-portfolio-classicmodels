d76e4a40c2158b7d9ada546ba55b51f891b0c3b7
kroliktm <milenapn@wp.pl>
2025-12-12 16:58:52 +0100

    Create product analysis SQL file

---
+++ sql/02_products.sql
@@ -0,0 +1,8 @@
+-- ================================
+-- PRODUCT ANALYSIS
+-- File: 02_products.sql
+-- Database: classicmodels
+-- Author: Milena
+-- ================================
+
+USE classicmodels;
