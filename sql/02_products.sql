d70235a59f4dc758243d82a98fb8a9b93078ec92
kroliktm <milenapn@wp.pl>
2025-12-12 17:15:34 +0100

    Update 02_products.sql

--- sql/02_products.sql
+++ sql/02_products.sql
@@ -6,3 +6,43 @@
 -- ================================
 
 USE classicmodels;
+-- ------------------------------------------------
+-- P1: Top products by revenue
+-- Business Question:
+-- Which products generate the highest revenue?
+-- ------------------------------------------------
+
+SELECT
+    p.productCode,
+    p.productName,
+    p.productLine,
+    ROUND(SUM(od.quantityOrdered * od.priceEach), 2) AS total_revenue
+FROM products p
+JOIN orderdetails od USING (productCode)
+GROUP BY
+    p.productCode,
+    p.productName,
+    p.productLine
+ORDER BY
+    total_revenue DESC
+LIMIT 10;
+-- ------------------------------------------------
+-- P2: Worst-selling products (including products with zero sales)
+-- Business Question:
+-- Which products generate the lowest revenue?
+-- ------------------------------------------------
+
+SELECT
+    p.productCode,
+    p.productName,
+    p.productLine,
+    ROUND(IFNULL(SUM(od.quantityOrdered * od.priceEach), 0), 2) AS total_revenue
+FROM products p
+LEFT JOIN orderdetails od USING (productCode)
+GROUP BY
+    p.productCode,
+    p.productName,
+    p.productLine
+ORDER BY
+    total_revenue ASC
+LIMIT 10;
