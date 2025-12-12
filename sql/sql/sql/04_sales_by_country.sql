eed44336ba1c42cca4339bc5c8d5f211c3148617
kroliktm <milenapn@wp.pl>
2025-12-12 18:53:27 +0100

    Add sales percentage share by country query
    Added a query to calculate the percentage share of sales by country.

--- sql/sql/sql/04_sales_by_country.sql
+++ sql/sql/sql/04_sales_by_country.sql
@@ -8,3 +8,26 @@ GROUP BY
     c.country
 ORDER BY 
     total_revenue DESC;
+
+-- ------------------------------------------------
+-- C2: Percentage share of sales by country
+-- Business Question:
+-- What percentage of total sales does each country contribute?
+-- ------------------------------------------------
+
+SELECT 
+    c.country,
+    ROUND(SUM(od.quantityOrdered * od.priceEach), 2) AS country_revenue,
+    ROUND(
+        SUM(od.quantityOrdered * od.priceEach)
+        / SUM(SUM(od.quantityOrdered * od.priceEach)) OVER () * 100,
+        2
+    ) AS revenue_share_percent
+FROM customers c
+JOIN orders o USING (customerNumber)
+JOIN orderdetails od USING (orderNumber)
+GROUP BY 
+    c.country
+ORDER BY 
+    revenue_share_percent DESC;
+
